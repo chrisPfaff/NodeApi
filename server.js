7916455ea6748d9c64e17da27a9c7c2025ab1196
Chris Pfaff <chrispfaff10@gmail.com>
2017-12-07 17:18:51 -0500

    added db.config

--- server.js
+++ server.js
@@ -3,14 +3,23 @@ const path = require('path');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 
+const db = require('./config/db');
+
 const app = express();
 
 const port = 8080;
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-require('./app/routes')(app, {});
+MongoClient.connect(db.url, (err, database) => {
+  if (err) {
+    return console.log(err);
 
-app.listen(port, () => {
-  console.log(`we are listening on port ${port}`);
-});
+    require('./app/routes')(app, {});
+
+    app.listen(port, () => {
+      console.log(`we are listening on port ${port}`);
+    });
+    
+  }
+})
