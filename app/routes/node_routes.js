60f9d98765f6b01ae3160e3628c6733edea34750
Chris Pfaff <chrispfaff10@gmail.com>
2017-12-08 13:33:32 -0500

    created put and delete routes

--- app/routes/node_routes.js
+++ app/routes/node_routes.js
@@ -23,6 +23,29 @@ module.exports = (app, db) => {
     })
   })
 
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    db.collection('notes').remove({"_id": ObjectID(id)}, (err, item) => {
+      if (err) {
+        res.send({error: 'An error happened'});
+      } else {
+        res.send(`note ${id} is deleted`);
+      }
+    })
+  })
+
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const note = {text: req.body.body, title: req.body.title};
+    db.collection('notes').update({"_id": ObjectID(id)}, note, (err, item) => {
+      if (err) {
+        res.send({error: 'An error happened'});
+      } else {
+        res.send(item);
+      }
+    })
+  })
+
   app.get('/', (req, res) => {
     res.send('hello');
   })
